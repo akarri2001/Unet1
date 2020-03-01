2a61ad6711c3072277e63c6bb3eb7b66f55055fe
akarri2001 <61641147+akarri2001@users.noreply.github.com>
2020-02-29 21:53:15 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/file/d/1-0HSyxEEu6hL6vdw6k9XMlULQ53JLJAu/view?usp=sharing'
+export_file_name = 'camvid-stage-1.pth'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Void', 'Background', 'MastCell']
 path = Path(__file__).parent
 
 app = Starlette()
